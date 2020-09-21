77351fbf77a215cad4929ebfb396690dbf9d2869
abayomi igwubor <igwubor@outlook.com>
2020-09-21 15:32:11 +0100

    addedShopping cart to code

---
+++ BulkyBook.DataAccess/Repository/OrderHeaderRepository.cs
@@ -0,0 +1,21 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+		public OrderHeaderRepository(ApplicationDbContext db): base(db)
+		{
+            _db = db;
+		}
+        
+    }
+}
